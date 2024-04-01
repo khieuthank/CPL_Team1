047d0c403702d76840ab36490d6ecd121fc8da0a
vuhuy301 <danhhuy8b@gmail.com>
2024-04-01 13:36:02 +0700

    update article detail

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,11 +12,11 @@ function App() {
   return (
     <BrowserRouter>
     <Header></Header>
-    <ArticleDetail></ArticleDetail>
     <Routes>
     <Route path="/" element={<Articles></Articles>} />
     <Route path="/users/login" element={<Login></Login>} />
     <Route path="/users/register" element={<Register></Register>} />
+    <Route path="/article/:slug" element={<ArticleDetail></ArticleDetail>}/>
     </Routes>
     </BrowserRouter>
   );
