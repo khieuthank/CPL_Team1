73403fde272bea62f8c9b6060365e69203f177b4
TongQuangHung102 <hungjob102@gmail.com>
2024-04-01 11:01:02 +0700

    Submit ArticleDetail
    Submit ArticleDetail lan 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Header from './components/app/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Articles from './components/articles/Articles';
-
+import ArticleDetail from './components/articles/ArticleDetail';
 function App() {
   return (
     <BrowserRouter>
     <Header></Header>
+    <ArticleDetail></ArticleDetail>
     <Routes>
     <Route path="/" element={<Articles></Articles>} />
     </Routes>
