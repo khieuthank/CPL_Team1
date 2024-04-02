d96d836de3348620b990e16a22d0970340e1a1fe
Trần Trung Kiên <trungkien2981412@gmail.com>
2024-04-02 10:28:13 +0700

    header

--- my-app/src/components/app/Header.jsx
+++ my-app/src/components/app/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
-
+import Settings from '../profile/Settings';
 const Header = () => {
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token');
+        if (storedToken) {
+            setToken(storedToken);
+        }
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        setToken('');
+    };
+
     return (
         <nav className="navbar navbar-light">
             <div className="container">
@@ -20,37 +32,38 @@ const Header = () => {
                 >
                     Conduit
                 </Link>
-
-
-
                 <div className="ml-auto">
-                    <ul className="nav navbar-nav d-flex flex-row" >
-                        <li className="nav-item" style={{
-                            marginLeft: '15px'
-                        }}>
-                            <Link className="nav-link" to="/" >Home</Link>
+                    <ul className="nav navbar-nav d-flex flex-row">
+                        <li className="nav-item" style={{ marginLeft: '15px' }}>
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
-                        <li className="nav-item" style={{
-                            marginLeft: '15px'
-                        }}>
-
-                            <Link className="nav-link" to="/users/login">Sign In</Link>
-
-                        </li>
-                        <li className="nav-item" style={{
-                            marginLeft: '15px'
-                        }}>
-
-                            <Link className="nav-link" to="/users/register">Sign Up</Link>
-
-                        </li>
+                        {token ? (
+                            <>
+                                <li className="nav-item" style={{ marginLeft: '15px' }}>
+                                    <Link className="nav-link" to="/settings">Settings</Link>
+                                </li>
+                                <li className="nav-item" style={{ marginLeft: '15px' }}>
+                                    <Link className="nav-link" to="/new-article">New Article</Link>
+                                </li>
+                                <li className="nav-item" style={{ marginLeft: '15px' }}>
+                                    <button className="nav-link btn btn-link" onClick={handleLogout}>Logout</button>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="nav-item" style={{ marginLeft: '15px' }}>
+                                    <Link className="nav-link" to="/users/login">Sign In</Link>
+                                </li>
+                                <li className="nav-item" style={{ marginLeft: '15px' }}>
+                                    <Link className="nav-link" to="/users/register">Sign Up</Link>
+                                </li>
+                            </>
+                        )}
                     </ul>
                 </div>
             </div>
         </nav>
-
     );
 };
-
 
 export default Header;
